d17f480535272f08680a04caccaf7aec0d7f4e49
Santi Bascal <sbascalcorrea@gmail.com>
2023-09-12 13:34:07 -0300

    juego terminado, proyecto listo

--- main.js
+++ main.js
@@ -9,13 +9,34 @@ function restar(){
     console.log(num1 - num2);
 }
 
-let inicio = prompt("calculadora");
+let inicio = prompt("calculadora o juego");
 
 if(inicio === "calculadora"){
     let signo = prompt("sumar o restar" );
     if(signo === "sumar"){
-        sumar()
+        sumar();
     }else if(signo === "restar"){
-        restar()
+        restar();
+    }else{
+        console.log("error 404, solo se sumar o restar");
     }
+}else if(inicio === "juego"){
+    let jugador1 = prompt("ingrese un nombre")
+    let jugador2 = prompt("ingrese un nombre")
+    const valorJugador1 = 1
+    const valorJugador2 = 2
+    for (let x=1; x<=5; x=x+1){
+        let moneda = Math.ceil(Math.random()*2);
+        if(moneda === valorJugador1){
+            console.log("Gano jugador " + jugador1)
+        }else if(moneda === valorJugador2){
+            console.log("Gano jugador " + jugador2)
+        }else{
+            console.log("empate")
+        }
+        console.log("--------------------")
+    }
+
+}else{
+    console.log("error 404, solo se calcular o jugar");
 }
